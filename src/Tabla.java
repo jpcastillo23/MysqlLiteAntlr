32613e8e9f2c3d605076d9453bcf7d9ef7ac5260
jpcastillo23 <josecastillo.ec09@gmail.com>
2015-04-19 16:58:03 -0600

    Ultimo Ocmmint

--- src/Tabla.java
+++ src/Tabla.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
@@ -38,9 +39,21 @@ import com.google.gson.reflect.TypeToken;
  * */
 
 public class Tabla  implements java.io.Serializable{
-	
+	/**
+	 * Lista que maneja las filas con datos
+	 * */
 	private List<Fila> arreglo_filas = new ArrayList<Fila>();
+	/**
+	 * Lista que maneja las columnas (nuestra sintaxis de tabla)
+	 * */
+	private List<NewConstrain> columnas_tabla = new ArrayList<NewConstrain>();
+	/**
+	 * siempre es necesari mantener referencia a como se llama este objeto
+	 * */
 	private static String nombre_table;
+	/**
+	 * Constructor de nuestros archivos JSon
+	 * */
 	private Gson gson = new Gson();
 	
 	public Tabla(){
@@ -63,7 +76,19 @@ public class Tabla  implements java.io.Serializable{
 	public static void saveTable( Tabla tabla, String where){
 		
 		//primero salva guarto los archivos .json y sus ubicaciones
-		
+		/***
+		 * revisa esto Carlos Barrios.  
+		 * PD: si no miras esto significa que ni siquiera lo abriste para revisar.
+		 * mañana entregamos y no tenemos ni el 30% del programa hecho. 
+		 * Favor ayudame a completar. ya me canse de estar en este progarma. 
+		 * Echale ganas por favor. Gracias. 
+		 * 
+		 * PD:  Contesta tu celular por favor, me da la impresion que obvias 
+		 * contestarme, y lo unico que quiero es que al menos esta cochinada 
+		 * haga algo.
+		saveJsonObjectsColumnas();
+		saveJsonObjectsFilas();
+		  //  */
 		
 		//luego salvaguardo mi objeto tabla para abrirlo despues
 		try{
@@ -90,30 +115,57 @@ public class Tabla  implements java.io.Serializable{
 		
 	}
 	/**
+	 * 
+	 * METODOS PARA MANEJO DE JSON DENTRO DE NUESTRA TABLA 
+	 * 
+	 *   ------------------     JSON    ---------------------
+	 *        
+	 *        SALVAR EL OBJETO TIPO JSON  DE LAS FILAS
 	 * what could be:
-	 * 	-column   ->   "<table_name>column.json"
-	 *  -filadata ->   "<table_name>data.json"
+	 * 	-column   ->   "<table_name>column.json"	   COLUMAS
+	 *  -filadata ->   "<table_name>data.json"      FILAS
 	 *  -restric	  ->	   "<table_name>restric.json"
 	 * */
 	
 	//	private List<Fila> arreglo_filas = new ArrayList<Fila>();
-	public static void saveJsonObjectsFilas(Object a_guardar, String where,String what, List<Fila> arreglo_filas_ext){
+	public static void saveJsonObjectsFilas( String where, List<Fila> arreglo_filas_ext){
 		Writer writer = null;
 		Type tipoListaFilas = new TypeToken<List<Fila>>(){}.getType();
-		Type tipoListaEmpleados = new TypeToken<List<Fila>>(){}.getType();
-		externo.getClass()
 		try {
 			writer = new OutputStreamWriter( 
-					 new FileOutputStream(where + File.separator+nombre_table+"filas.json") ,
+					 new FileOutputStream(where + File.separator +nombre_table+"filas.json") ,
 					"UTF-8");
 		}catch(Exception f){
-			
+			f.printStackTrace();
 		}
 	        Gson gson = new GsonBuilder().create();
-	        gson.toJson(a_guardar, tipo_clase);
-	        writer.close();
+	        gson.toJson(arreglo_filas_ext, tipoListaFilas);
+	        try {
+				writer.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
     }
 	
-	public static 
+	public static void saveJsonObjectsColumnas(String where,String what, List<NewConstrain> arreglo_filas_ext){
+		Writer writer = null;
+		Type tipoListaColumnas = new TypeToken<List<NewConstrain>>(){}.getType();
+		try {
+			writer = new OutputStreamWriter( 
+					 new FileOutputStream(where + File.separator +nombre_table+"filas.json") ,
+					"UTF-8");
+		}catch(Exception f){
+			f.printStackTrace();
+		}
+	        Gson gson = new GsonBuilder().create();
+	        gson.toJson(arreglo_filas_ext, tipoListaColumnas);
+	        try {
+				writer.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    }
 }
 
